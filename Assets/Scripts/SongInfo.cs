64aa4b8e6277bb2d50b575d5ed916856e0351643
Justin Patterson <jhpatt33@gmail.com>
2021-03-27 17:15:51 -0400

    Basic logic for menu screen

---
+++ Assets/Scripts/SongInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SongInfo", menuName = "Song/InfoObject")]
+public class SongInfo : ScriptableObject
+{
+    public string songName;
+    public string artist;
+    public float duration;
+    public AudioClip musicClip;
+}
